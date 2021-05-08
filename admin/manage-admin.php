46de4048b9423cd170019e6429aec3d9f56cc438
Mohammed Sibahi <sibahi@live.com>
2021-05-09 01:47:59 +0300

    display message upon deleting an admin successfully

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -7,7 +7,20 @@
     <div class="wrapper">
         <h1>Manage Admin</h1>
         <br /><br />
+        <?php
 
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add']; // displaying session message
+            unset($_SESSION['add']); // removing session message
+        }
+
+        if (isset($_SESSION['delete'])) {
+            echo $_SESSION['delete'];
+            unset ($_SESSION['delete']);
+        }
+
+        ?>
+        <br /><br /><br />
         <!--Button to Add Admin-->
 
         <a href="add-admin.php" class="btn-primary">Add Admin</a>
@@ -21,36 +34,52 @@
                 <th>Username</th>
                 <th>Actions</th>
             </tr>
-            <tr>
-                <td>1. </td>
-                <td>Moahmmed Sibahi</td>
-                <td>Mohammed Sibahi</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
+            <?php
+            // Query to get all admins 
+            $sql = "SELECT * FROM tbl_admin";
+            // execute the query 
+            $res = mysqli_query($conn, $sql);
 
-                </td>
-            </tr>
+            // check whether the Query is executed or not
+            if ($res == TRUE) {
+                // count rows to check we have data in the database or not
+                $count = mysqli_num_rows($res); // function to get all the rows in the data base
 
-            <tr>
-                <td>2. </td>
-                <td>Moahmmed Sibahi</td>
-                <td>Mohammed Sibahi</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                $sn = 1; // create a varialbe and assign the value 
 
-            <tr>
-                <td>3. </td>
-                <td>Moahmmed Sibahi</td>
-                <td>Mohammed Sibahi</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                // check the num of rows 
+                if ($count > 0) {
+                    // we have data in the database
+                    while ($rows = mysqli_fetch_assoc($res)) {
+                        // using while loop to get all the data from the database
+                        // and while loop will run as long as we have data in the database
+
+                        // get individual data 
+                        $id = $rows['id'];
+                        $full_name = $rows['full_name'];
+                        $username = $rows['username'];
+
+                        // display the values in our table 
+            ?>
+                        <tr>
+                            <td><?php echo $sn++; ?>. </td>
+                            <td><?php echo $full_name; ?></td>
+                            <td><?php echo $username; ?></td>
+                            <td>
+                                <a href="#" class="btn-secondary">Update Admin</a>
+                                <a href="<?php echo SITEURL ; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
+
+                            </td>
+                        </tr>
+
+            <?php
+
+                    }
+                } else {
+                    // We do not have data in the database
+                }
+            }
+            ?>
 
 
         </table>
