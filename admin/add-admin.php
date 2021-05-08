91a2a57df9c3f5b84aafc06c3eac55c894936519
Mohammed Sibahi <sibahi@live.com>
2021-05-08 23:24:46 +0300

    create a session

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -58,7 +58,33 @@ $sql = "INSERT INTO tbl_admin SET
     password = '$password'
 ";
 
-echo $sql;
+//3. Execute query and save data in the database
+$conn = mysqli_connect('localhost', 'root', '') or die (mysqli_error($conn));
+$db_select = mysqli_select_db($conn, 'food-order') or die(mysqli_error($conn));
+
+$res = mysqli_query($conn, $sql) or die (mysqli_error($conn));
+
+//4. check whether the (query is executed) data is inserted or not
+
+if ($res == TRUE) {
+    // data inserted
+    // echo "Data inserted";
+    // create a session variable to display message 
+    $_SESSION['add'] = "Admin Added Successfully";
+    // Redirect page to manage admin
+    header("location:".SITEURL.'admin/manage-admin.php'); 
+
+} else {
+    //failed to insert data
+    // echo "Failed to insert data";
+ // create a session variable to display message 
+ $_SESSION['add'] = "Failed to Add Admin";
+ // Redirect page to add admin
+ header("location:".SITEURL.'admin/add-admin.php'); 
+
+
+}
+
 } 
 
 ?>
