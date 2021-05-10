6f0b8ce498a647ac2f59de3e65de2690424f76e9
Mohammed Sibahi <sibahi@live.com>
2021-05-11 01:23:52 +0300

    create the login page

---
+++ admin/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <link
+  rel="stylesheet"
+  href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css"
+/>
+    <title>Login - Food Order System</title>
+</head>
+
+<body>
+
+    <div class="login ms-depth-16">
+
+        <h1 class="text-center">Login</h1>
+        <br /><br />
+        <!--Login form starts here-->
+        <form action="" method="POST" class="text-center">
+            Username:
+            <br />
+            <input class="ms-depth-16" type="text" name="username" placeholder="Enter Username" />
+            <br /><br />
+            Password:
+            <br />
+            <input class="ms-depth-16" type="passowrd" name="password " placeholder="Enter Password" />
+            <br /><br />
+            <input type="submit" value="Login" class="btn-primary" />
+            <br /><br />
+        </form>
+
+
+        <!--Login form ends here-->
+
+        <p class="text-center">Created by - <a href="https://github.com/Mohammed-Sibahi">Mohammed Sibahi</a></p>
+    </div>
+
+</body>
+
+</html>
