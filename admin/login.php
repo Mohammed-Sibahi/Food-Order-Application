6463044da510b0b743c55464782223cefbca4c7c
Mohammed Sibahi <sibahi@live.com>
2021-05-11 03:33:26 +0300

    Write the query for authenticating username and password

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,5 @@
+<?php include('../config/constants.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,10 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/admin.css">
-    <link
-  rel="stylesheet"
-  href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css"
-/>
+    <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css" />
     <title>Login - Food Order System</title>
 </head>
 
@@ -27,9 +26,9 @@
             <br /><br />
             Password:
             <br />
-            <input class="ms-depth-16" type="passowrd" name="password " placeholder="Enter Password" />
+            <input class="ms-depth-16" type="password" name="password" placeholder="Enter Password" />
             <br /><br />
-            <input type="submit" value="Login" class="btn-primary" />
+            <input type="submit" name="submit" value="Login" class="btn-primary" />
             <br /><br />
         </form>
 
@@ -42,3 +41,38 @@
 </body>
 
 </html>
+
+<?php
+// check whether the submit button is clicked or not
+if (isset($_POST['submit'])) {
+    // process for login
+    // 1. Get the data from the login form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
+// 2. sql to check whether the user and username and password exist
+$sql = "SELECT * FROM tbl_admin WHERE username = '$username' AND password = '$password' ";
+
+// 3. execute the query 
+
+$res = mysqli_query($conn, $sql);
+
+// 4. count rows to check whether the user exists or not 
+
+$count = mysqli_num_rows($res);
+
+
+if ($count == 1) {
+    // user is available and login success
+    $_SESSION['login'] = "<div class='success'>Login successful!</div>";
+    // redirect to homepage / dashboard 
+    header('location' . SITEURL . 'admin/');
+} else {
+    //user not available and login fail
+    $_SESSION['login'] = "<div class='error'>Login failed! Username or password did not match!</div>";
+
+    // redirect to homepage / dashboard 
+    header('location' . SITEURL . 'admin/login.php');
+}
+
+?>
