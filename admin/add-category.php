e6c606bd573791c7b6bb5f8ec77ce66b260bb8ce
Mohammed Sibahi <sibahi@live.com>
2021-05-12 23:57:04 +0300

    upload images to database

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,6 +1,5 @@
-<?php
-include('partials/menu.php');
-?>
+<?php include('partials/menu.php'); ?>
+
 <div class="main-content">
     <div class="wrapper">
         <h1>Add Category</h1>
@@ -10,23 +9,37 @@ include('partials/menu.php');
         <?php
 
         if (isset($_SESSION['add'])) {
+            
             echo $_SESSION['add'];
+            
             unset($_SESSION['add']);
         }
+        if (isset($_SESSION['upload'])) {
+            
+            echo $_SESSION['upload'];
+            
+            unset($_SESSION['upload']);
+        }
+
         ?>
 
         <br /><br />
-        
+
 
         <!--Add Category Form Starts Here-->
-        <form action="" method="POST">
+        <form action="" method="POST" enctype="multipart/form-data">
             <table class="tbl-30">
 
                 <tr>
                     <td>Title:</td>
                     <td><input type="text" name="title" placeholder="Category Title" /></td>
                 </tr>
-
+                <tr>
+                    <td>Select Image: </td>
+                    <td>
+                        <input type="file" name="image" />
+                    </td>
+                </tr>
                 <tr>
                     <td>Featured: </td>
                     <td>
@@ -80,10 +93,54 @@ include('partials/menu.php');
 
                 $active = $_POST['No'];
             }
+
+            // check whether the image is selected and set the value for image name accordingly
+
+            // print_r($_FILES['image']);
+
+            //die(); // break the code here
+
+            if (isset($_FILES['image']['name'])) {
+                // upload the image
+                // to upload the image, we need image name, source path and destination path
+                $image_name = $_FILES['image']['name'];
+
+                $source_path = $_FILES['image']['tmp_name'];
+
+                $destination_path = "../images/category" . $image_name;
+                //upload the image
+                $upload = move_uploaded_file($source_path, $destination_path);
+
+                // check whether the image is uploaded or not
+                // and if the image is not uploaded then we'll stop the process and redirect with error message
+                if ($upload == false) {
+                    // set message
+
+                    $_SESSION['upload'] = "<div class='error'>Failed to Upload the Image</div>";
+
+                    // redirect to add category page
+                    header('location:' . SITEURL . 'admin/add-category.php');
+
+                    // stop the process
+                    die();
+                }
+            } else {
+
+                // don't upload the image and set the image name value as blank
+                $image_name = "";
+            }
+
+
+
             //2. Create SQL query to insert categories into database
             $sql = "INSERT INTO tbl_category SET 
+            
             title = '$title',
+
+           image_name = '$image_name',
+
             featured = '$featured',
+            
             active = '$active'
             
             ";
@@ -116,6 +173,4 @@ include('partials/menu.php');
 </div>
 
 
-<?php
-include('partials/footer.php');
-?>
+<?php include('partials/footer.php'); ?>
