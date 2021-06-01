ce034742b916e6a9b125a3c3a3e339d35a9f22b8
Mohammed Sibahi <sibahi@live.com>
2021-06-01 04:24:35 +0300

    display foods by the category selected

--- category-foods.php
+++ category-foods.php
@@ -1,133 +1,127 @@
 <?php include('partials-front/menu.php'); ?>
 
+<?php
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <h2>Foods on <a href="#" class="text-white">"Category"</a></h2>
+// check whether id is passed or not
+if (isset($_GET['category_id'])) {
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+    // category id is set and get the id
+    $category_id = $_GET['category_id'];
+
+    // get the category title based on category id
+    $sql = "SELECT title FROM tbl_category WHERE id = $category_id ";
+
+    // execute the query 
+    $res = mysqli_query($conn, $sql);
+
+    // get the value from the database
+    $row = mysqli_fetch_assoc($res);
+
+    // get the title
+    $category_title = $row['title'];
+} else {
+
+    // category not passed
+    // redirect to home page
+    header('location:' . SITEURL);
+}
+
+?>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
+
+        <h2>Foods on <a href="#" class="text-white">"<?php echo $category_title; ?>"</a></h2>
+
+    </div>
+</section>
+<!-- FOOD SEARCH Section Ends Here -->
 
 
 
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+<!-- FOOD Menu Section Starts Here -->
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Food Menu</h2>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+        <?php
+
+        // create sql query to get foods based on selected category 
+        $sql2 = "SELECT * FROM tbl_food WHeRE category_id = $category_id";
+
+        // execute the query
+        $res2 = mysqli_query($conn, $sql2);
+
+        // count the rows 
+        $count2 = mysqli_num_rows($res2);
+
+        // check whether food is available or not 
+        if ($count2 > 0) {
+
+            // food is available 
+            while ($row2 = mysqli_fetch_assoc($res2)) {
+                $title = $row2['title'];
+                $price = $row2['price'];
+                $description = $row2['description'];
+                $image_name = $row2['image_name'];
+        ?>
+                <div class="food-menu-box">
+                    <div class="food-menu-img">
+                    <?php 
+
+                    // check whether image is available or not 
+                    if($image_name == "") {
+                        // image is not available 
+
+                        echo "<div class='error'>Image is not available</div>";
+                    } else {
+
+                        // image is available 
+                        ?>
+                        <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+
+                        <?php
+                    }
+                    
+                    ?>
+                    </div>
+
+                    <div class="food-menu-desc">
+                        <h4><?php echo $title; ?></h4>
+                        <p class="food-price">$<?php echo $price; ?></p>
+                        <p class="food-detail">
+                            <?php echo $description; ?>
+                        </p>
+                        <br>
+
+                        <a href="#" class="btn btn-primary">Order Now</a>
+                    </div>
                 </div>
 
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Smoky Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Nice Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-momo.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Chicken Steam Momo</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
 
 
-            <div class="clearfix"></div>
+        <?php
+            }
+        } else {
 
-            
+            // food is not available 
+            echo "<div class='error'>Food is not available.</div>";
+        }
 
-        </div>
 
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
 
-    <?php include('partials-front/footer.php'); ?>
+
+
+        ?>
+
+
+        <div class="clearfix"></div>
+
+
+
+    </div>
+
+</section>
+<!-- fOOD Menu Section Ends Here -->
+
+<?php include('partials-front/footer.php'); ?>
